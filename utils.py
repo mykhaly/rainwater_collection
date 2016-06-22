4ff77b0dc96b7afc810eacbd932d8a71e8468e0f
Yurii Mykhalchuk <yurkamykhalchuk@gmail.com>
2016-06-22 21:44:36 +0300

    Clean shit

--- utils.py
+++ utils.py
@@ -1,77 +1,32 @@
-#def min_neigh(row, col, arr):
-#    last = len(arr) - 1
-#    result = []
-#    if row != last and row != 0:
-#        if col != last and col != 0:
-#            result.append((row - 1, col))
-#            result.append((row - 1, col + 1))
-#            result.append((row, col + 1))
-#            result.append((row + 1, col + 1))
-#            result.append((row + 1, col))
-#            result.append((row + 1, col - 1))
-#            result.append((row, col - 1))
-#            result.append((row - 1, col - 1))
-#        elif col == last:
-#            result.append((row - 1, col))
-#            result.append((row + 1, col))
-#            result.append((row + 1, col - 1))
-#            result.append((row, col - 1))
-#            result.append((row - 1, col - 1))
-#        elif col == 0:
-#            result.append((row - 1, col))
-#            result.append((row - 1, col + 1))
-#            result.append((row, col + 1))
-#            result.append((row + 1, col + 1))
-#            result.append((row + 1, col))
-#    elif row == last:
-#        if col != last and col != 0:
-#
-#    elif row == 0:
-#        if col != last and col != 0:
-#            result.append((row, col + 1))
-#            result.append((row + 1, col + 1))
-#            result.append((row + 1, col))
-#            result.append((row + 1, col - 1))
-#            result.append((row, col - 1))
-#        elif col == last:
-#            result.append((row + 1, col))
-#            result.append((row + 1, col - 1))
-#            result.append((row, col - 1))
-#        elif col == 0:
-#            result.append((row, col + 1))
-#            result.append((row + 1, col + 1))
-#            result.append((row + 1, col))
-#
-def mooo(row, col, arr):
+def minimal_neighbours(row, col, arr):
     last = len(arr) - 1
-    result_ind = []
+    indices = []
     if row != 0:
-        result_ind.append((row - 1, col))
+        indices.append((row - 1, col))
     if row != 0 and col != last:
-        result_ind.append((row - 1, col + 1))
+        indices.append((row - 1, col + 1))
     if col != last:
-        result_ind.append((row, col + 1))
+        indices.append((row, col + 1))
     if row != last and col != last:
-        result_ind.append((row + 1, col + 1))
+        indices.append((row + 1, col + 1))
     if row != last:
-        result_ind.append((row + 1, col))
+        indices.append((row + 1, col))
     if row != last and col != 0:
-        result_ind.append((row + 1, col - 1))
+        indices.append((row + 1, col - 1))
     if col != 0:
-        result_ind.append((row, col - 1))
+        indices.append((row, col - 1))
     if row != 0 and col != 0:
-        result_ind.append((row - 1, col - 1))
+        indices.append((row - 1, col - 1))
 
-    result_val = []
-    for element in result_ind:
-        result_val.append(arr[element[0]][element[1]])
-    minimum = min(result_val)
-    minimum = min(minimum, arr[row][col])
-    result_result = []
-    for index, element in enumerate(result_val):
-        if element == minimum:
-            result_result.append(result_ind[index])
-    return result_result
+    values = []
+    for element in indices:
+        values.append(arr[element[0]][element[1]])
+    min_val = min(values, arr[row][col])
+    result = []
+    for index, element in enumerate(values):
+        if element == min_val:
+            result.append(indices[index])
+    return result
 
 foo = [[x for x in range (3)] for y in range(3)]
 for i in range(3):
