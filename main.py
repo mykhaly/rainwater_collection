9f240ee40ef99392df431ec7447ead91d9d417c8
Yurii Mykhalchuk <yurkamykhalchuk@gmail.com>
2016-06-23 03:26:55 +0300

    WIP

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from .utils import minimal_neighbours
+from utils import minimal_neighbours, neighbours
 
 class C:
     def __init__(self, terrain_height=0, water_height=0):
         self.t = terrain_height
         self.w = water_height
 
+    @property
     def height(self):
         return self.t + self.w
 
@@ -14,14 +15,50 @@ class C:
     __repr__ = __str__
 
 
-array = []
-for i in range(5):
-    array.append([])
-    for j in range(5):
-        array[i].append(C(i + 1, 0))
+foo = \
+[
+    [C(1, 1.2), C(-2, 1), C(2, 1)],
+    [C(-2, 1), C(1, 1), C(2, 1)],
+    [C(1, 1), C(1, 1), C(2, 1)]
+]
 
+for i in range(3):
+    print foo[i]
 
-for i in range(5):
-    for l in range(5):
-        print array[i][l], "\t",
+for i in range(3):
+    for j in range(3):
+        print foo[i][j].height,
     print "\n"
+
+current_neighbours = neighbours(0, 0, foo)
+curr_min_neighb = minimal_neighbours(0, 0, foo)
+curr_min_neighb_diffs = []
+print "NEIGHBours  ", current_neighbours
+print "MIN NEIGHB  ", curr_min_neighb
+
+for min_neighb in curr_min_neighb:
+    min_neighb_neighb = neighbours(min_neighb[0], min_neighb[1], foo)
+    shared_neighb = list(set.intersection(set(current_neighbours), set(min_neighb_neighb)))
+    minimal_diff = float("inf")
+
+    for shrd_nghbr in shared_neighb:
+        if foo[shrd_nghbr[0]][shrd_nghbr[1]].height - foo[min_neighb[0]][min_neighb[1]].height < minimal_diff and foo[shrd_nghbr[0]][shrd_nghbr[1]].height - foo[min_neighb[0]][min_neighb[1]].height != 0:
+            minimal_diff = foo[shrd_nghbr[0]][shrd_nghbr[1]].height - foo[min_neighb[0]][min_neighb[1]].height
+    curr_min_neighb_diffs.append(minimal_diff)
+print "curr_min_neighb_diffs  ", curr_min_neighb_diffs
+diff_sum = sum(curr_min_neighb_diffs)
+the_most_min_diff = min(curr_min_neighb_diffs)
+if the_most_min_diff * len(curr_min_neighb) > foo[0][0].w:
+    amount_of_water = foo[0][0].w / len(curr_min_neighb)
+    for min_neighb in curr_min_neighb:
+        foo[min_neighb[0]][min_neighb[1]].w += amount_of_water
+        foo[0][0].w -= amount_of_water
+
+for i in range(3):
+    print foo[i]
+
+for i in range(3):
+    for j in range(3):
+        print foo[i][j].height,
+    print "\n"
+
--- utils.py
+++ utils.py
@@ -1,4 +1,19 @@
 def minimal_neighbours(row, col, arr):
+    indices = neighbours(row, col, arr)
+    values = []
+    for element in indices:
+        values.append(arr[element[0]][element[1]].height)
+    min_val = min(values)
+    min_val = min(min_val, arr[row][col].height)
+    #print "values  ", values
+    #print "min value  ", min_val
+    result = []
+    for index, element in enumerate(values):
+        if element == min_val:
+            result.append(indices[index])
+    return result
+
+def neighbours(row, col, arr):
     last = len(arr) - 1
     indices = []
     if row != 0:
@@ -17,22 +32,63 @@ def minimal_neighbours(row, col, arr):
         indices.append((row, col - 1))
     if row != 0 and col != 0:
         indices.append((row - 1, col - 1))
+    #print indices
+    return indices
 
-    values = []
-    for element in indices:
-        values.append(arr[element[0]][element[1]])
-    min_val = min(values, arr[row][col])
-    result = []
-    for index, element in enumerate(values):
-        if element == min_val:
-            result.append(indices[index])
-    return result
+def minimum_diff(curr_row, curr_col, min_row, min_col, arr):
+    foo = minimal_neighbours(curr_row, curr_col, arr)
+    bar = minimal_neighbours(min_row, min_col, arr)
+    print foo
+    print bar
+    quax = set.intersection(foo, bar)
+    print quax
 
-foo = [[x for x in range (3)] for y in range(3)]
-for i in range(3):
-    print foo[i]
 
-for i in range(3):
-    for j in range(3):
-        print i, j, "\t", mooo(i, j, foo)
-    print "\n"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def blabla(arr):
+#    size = len(arr)
+#    water_moved = False
+#    for i in range(size):
+#        for j in range(size):
+#            water_moved = move_water(i, j, arr)
+#    if water_moved:
+#        return blabla(arr)
+#    return arr
+#
+#def move_water(row, col, arr):
+#    this_column_water_amount = arr[row][col].water
+#    this_column_height = arr[row][col].height
+#    min_neigh = minimal_neighbours(row, col, arr)
+#    how_many_water_could_be_moved_into_neighbour = []
+#    for neighbour in min_neigh:
+#        water_could_be_moved_into_current_neighbour = minimal_higher_neighbour(neighbour[0], neighbour[1], arr)
+#    i need to find cells to move water into k
+#
+#def water_could_be_moved_into(row, col, arr):
+#    result = 0
+#    if minimal_higher_neighbour.height == arr[row][col].height:
+#        
